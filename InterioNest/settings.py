676686165d09df5de506c8cfdc5376029a01f2dd
Puja <pb2853313@gmail.com>
2025-07-09 19:27:56 +0530

    Add .gitignore and update settings.py and requirements.txt

--- InterioNest/settings.py
+++ InterioNest/settings.py
@@ -26,10 +26,14 @@ SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-=js4jf+xebv7*=q6iv$r0)
 
 
 # DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS", "localhost")]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "")
+ALLOWED_HOSTS = [host.strip() for host in ALLOWED_HOSTS.split(",") if host.strip()]
 
+if DEBUG:
+    ALLOWED_HOSTS += ['localhost', '127.0.0.1']
+    
 # Application definition
 
 INSTALLED_APPS = [
@@ -45,13 +49,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # whitenoise add
+    
 ]
 
 ROOT_URLCONF = 'InterioNest.urls'
@@ -121,12 +126,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'main/static'),
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 LOGIN_REDIRECT_URL = '/admin-dashboard/'
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
